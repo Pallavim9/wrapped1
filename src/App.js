2653d2fc4ec4994487c4c36c8df743c95c291ea9
Pallavim9 <pallum9@gmail.com>
2024-08-06 01:22:21 -0400

    Switching from BrowserRouter to HashRouter for github pages deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Wrapped from './components/Wrapped';
 import { useState } from 'react';
 import Login from './components/Login';
